f9a63ee66fd6f0c507579e6108d8c08ac5d7d20e
YSK722 <56831867+YSK722@users.noreply.github.com>
2021-12-25 19:18:04 +0900

    Add files via upload

---
+++ AutoTrade/gmocoin.py
@@ -0,0 +1,99 @@
+import hmac
+import hashlib
+import json
+import time
+from datetime import datetime
+
+import requests
+
+from utils.notify import send_message_to_line
+
+
+class GMOcoin(object):
+    def __init__(self, access_key, secret_key):
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.url = 'https://api.coin.z.com'
+
+    def _request(self, endpoint, pp='/public', params=None, method='GET'):
+        time.sleep(1)
+        timestamp = '{0}000'.format(
+            int(time.mktime(datetime.now().timetuple())))
+        body = json.dumps(params) if params else ''
+
+        text = timestamp + method + endpoint + body
+        sign = hmac.new(bytes(self.secret_key.encode('ascii')), bytes(
+            text.encode('ascii')), hashlib.sha256).hexdigest()
+
+        headers = {
+            "API-KEY": self.access_key,
+            "API-TIMESTAMP": timestamp,
+            "API-SIGN": sign
+        }
+
+        try:
+            url = self.url+pp+endpoint
+            if method == 'GET':
+                r = requests.get(url, headers=headers, params=params)
+            else:
+                r = requests.post(url, headers=headers, data=body)
+        except Exception as e:
+            send_message_to_line(e)
+            raise
+
+        return r.json()
+
+    def ticker(self):
+        endpoint = '/v1/ticker?symbol=XEM'
+        return self._request(endpoint=endpoint)
+
+    @property
+    def last(self):
+        return float(self.ticker()['data'][0]['last'])
+
+    @property
+    def ask(self):
+        return float(self.ticker()['data'][0]['ask'])
+
+    def trades(self, params=None):
+        endpoint = '/v1/trades?symbol=XEM'
+        return self._request(endpoint=endpoint, params=params)
+
+    def order_books(self, params=None):
+        endpoint = '/v1/orderbooks?symbol=XEM'
+        return self._request(endpoint=endpoint, params=params)
+
+    def balance(self):
+        pp = '/private'
+        endpoint = '/v1/account/assets'
+        return self._request(endpoint=endpoint, pp=pp)
+
+    @property
+    def position(self):
+        balance = self.balance()
+        if not balance.get('data'):
+            return {}
+        return {d['symbol']: d['amount'] for d in balance['data']}
+
+    def order(self, params):
+        pp = '/private'
+        endpoint = '/v1/order'
+        return self._request(endpoint=endpoint, pp=pp, params=params,
+                             method='POST')
+
+    def cancel(self, params):
+        pp = '/private'
+        endpoint = '/v1/cancelBulkOrder'
+        return self._request(endpoint=endpoint, pp=pp, params=params,
+                             method='POST')
+
+    def transaction(self):
+        endpoint = '/v1/trades?symbol=XEM'
+        return self._request(endpoint=endpoint)
+
+    @property
+    def ask_rate(self):
+        transaction = self.transaction()
+        ask_transaction = [d for d in transaction['data']['list']
+                           if d['side'] == 'BUY']
+        return float(ask_transaction[0]['price'])
