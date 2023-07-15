e362e9b9a0885230226d29e54ef79e7c4a64d76d
YSK722 <56831867+YSK722@users.noreply.github.com>
2023-07-15 12:45:05 +0900

    Update mainAll.py

--- mainAll.py
+++ mainAll.py
@@ -44,7 +44,11 @@ stack1, stack2 = 20, 192
 
 send_message_to_line('Start Auto Trading...')
 i, priceAtAsk = 0, 0
-Ticker = yfinance.Ticker('XEM-JPY')
+symbols = ['XEM', 'XRP', 'LTC', 'BCH', 'ETH', 'BTC']
+Tickers = []
+for s in symbols:
+    Tickers.append(yfinance.Ticker(f'{s}-JPY'))
+
 while True:
     time.sleep(interval)
     if i == 60*60*24/interval - 1:
@@ -54,47 +58,56 @@ while True:
         i += 1
 
     try:
-        df = Ticker.history(period='7d', interval='15m')
-        gmocoin.cancelBulkOrder({'symbols': ['XEM']})
-        posXEM = gmocoin.position['XEM']
+        gmocoin.cancelBulkOrder({'symbols': symbols})
+        positions = gmocoin.position
+        posJPY = positions['JPY']  # example for try
     except Exception as e:
         send_message_to_line(e)
         send_message_to_line('Server Maintenance')
         continue
+    for i in range(len(symbols)):
+        symbol = symbols[i]
+        try:
+            df = Tickers[i].history(period='3d', interval='15m')
+        except Exception as e:
+            send_message_to_line(e)
+            continue
+        
+        df['+2σ'], df['MA1'], df['-2σ'] = BBANDS(df['Close'], timeperiod=stack1, matype=MA_Type.EMA)
+        df['MA2'] = MA(df['Close'], timeperiod=stack2)
 
-    df['+2σ'], df['MA1'], df['-2σ'] = BBANDS(df['Close'], timeperiod=stack1, matype=MA_Type.EMA)
-    df['MA2'] = MA(df['Close'], timeperiod=stack2)
+        price = df['Close'].iloc[-1]
+        MA1 = df['MA1'].iloc[-1]
+        MA2 = df['MA2'].iloc[-1]
+        lstMA1 = df['MA1'].iloc[-2]
+        lstMA2 = df['MA2'].iloc[-2]
 
-    price = df['Close'].iloc[-1]
-    MA1 = df['MA1'].iloc[-1]
-    MA2 = df['MA2'].iloc[-1]
-    lstMA1 = df['MA1'].iloc[-2]
-    lstMA2 = df['MA2'].iloc[-2]
+        if positions[symbol] != '0':
+            if 1.02 * priceAtAsk < price or \
+                    df['+2σ'].iloc[-1] < price and priceAtAsk < price or \
+                    MA1 < MA2 and priceAtAsk < price:
+                params = {
+                    'symbol': symbol,
+                    'side': 'SELL',
+                    'executionType': 'LIMIT',
+                    'price': price,
+                    'size': positions[symbol]
+                }
+                gmocoin.order(params)
+                mpf.plot(df, mav=(stack1, stack2), style='yahoo', savefig=f'{symbol}.png')
+        else:
+            if MA2 < MA1 and lstMA1 < lstMA2 and (derivative() or derivative2()) or \
+                    price < df['-2σ'].iloc[-1]:
+                size = str(int(float(posJPY)/price/4))  # maybe causes error 4 -> len(symbols)
+                params = {
+                    'symbol': symbol,
+                    'side': 'BUY',
+                    'executionType': 'LIMIT',
+                    'price': price,
+                    'size': size
+                }
+                gmocoin.order(params)
+                priceAtAsk = price
+                mpf.plot(df, mav=(stack1, stack2), style='yahoo', savefig=f'{symbol}.png')
 
-    if posXEM != '0':
-        if 1.02 * priceAtAsk < price or \
-                df['+2σ'].iloc[-1] < price and priceAtAsk < price or \
-                MA1 < MA2 and priceAtAsk < price:
-            params = {
-                'symbol': 'XEM',
-                'side': 'SELL',
-                'executionType': 'LIMIT',
-                'price': price,
-                'size': positions['XEM']
-            }
-            gmocoin.order(params)
-            mpf.plot(df, mav=(stack1, stack2), style='yahoo', savefig='XEM.png')
-    else:
-        if MA2 < MA1 and lstMA1 < lstMA2 and (derivative() or derivative2()) or \
-                price < df['-2σ'].iloc[-1]:
-            size = str(int(0.95*float(positions['JPY'])/price))
-            params = {
-                'symbol': 'XEM',
-                'side': 'BUY',
-                'executionType': 'LIMIT',
-                'price': price,
-                'size': size
-            }
-            gmocoin.order(params)
-            priceAtAsk = price
-            mpf.plot(df, mav=(stack1, stack2), style='yahoo', savefig='XEM.png')
+        del df
