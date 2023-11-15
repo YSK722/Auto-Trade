e840748a6ab205e6e27c21617096ace00c4d4e52
YSK722 <56831867+YSK722@users.noreply.github.com>
2023-11-15 20:42:53 +0900

    Update mainAll.py

--- mainAll.py
+++ mainAll.py
@@ -11,24 +11,29 @@ from gmocoin import GMOcoin
 from utils.notify import send_message_to_line
 
 
-def derivative():
-    result = 0
-    for i in range(stack1):
-        if df['MA2'].iloc[-i-1] > df['MA2'].iloc[-i-2]:
-            result += 1
-        else:
-            result -= 1
-    return (result > 0)
+def calcPrice(symbol, price):
+    if symbol in []:
+        return f'{price:.3f}'
+    else:
+        return f'{price:.0f}'
 
 
-def derivative2():
-    result = 0
-    for i in range(stack1):
-        if df['MA2'].iloc[-i-1] + df['MA2'].iloc[-i-3] > 2 * df['MA2'].iloc[-i-2]:
-            result += 1
-        else:
-            result -= 1
-    return (result > 0)
+def calcSize(symbol, size):
+    if symbol in []:
+        size /= 4
+        return f'{size:.0f}'
+    elif symbol in []:
+        size /= 3
+        return f'{size:.1f}'
+    elif symbol in []:
+        size /= 2
+        return f'{size:.2f}'
+    elif symbol in []:
+        size /= 2
+        return f'{size:.3f}'
+    elif symbol in []:
+        size /= 2
+        return f'{size:.4f}'
 
 
 conf = configparser.ConfigParser()
@@ -39,75 +44,90 @@ SECRET_KEY = conf['gmocoin']['secret_key']
 
 gmocoin = GMOcoin(access_key=ACCESS_KEY, secret_key=SECRET_KEY)
 
-interval = 60*15
-stack1, stack2 = 20, 192
+interval = 15  # minites
+stack1, stack2, stack3 = 20, 192, 672
+period = (interval*stack3 + 60*24 - 1)//(60*24) + 1
 
 send_message_to_line('Start Auto Trading...')
-i, priceAtAsk = 0, 0
-symbols = ['XEM', 'XRP', 'LTC', 'BCH', 'ETH', 'BTC']
+isOrder = False
+symbols = []
+count, priceAtAsk = 0, [0] * len(symbols)
+positions = gmocoin.position
 Tickers = []
-for s in symbols:
+for i, s in enumerate(symbols):
+    if positions[s] != '0':
+        priceAtAsk[i] = float(gmocoin.ticker(s)['data'][0]['last'])
     Tickers.append(yfinance.Ticker(f'{s}-JPY'))
 
 while True:
-    time.sleep(interval)
-    if i == 60*60*24/interval - 1:
-        i = 0
+    time.sleep(60 * interval)
+    if count > 60*24/interval:
+        count = 0
         send_message_to_line('Auto Trading...')
     else:
-        i += 1
+        count += 1
 
-    try:
+    if isOrder:
         gmocoin.cancelBulkOrder({'symbols': symbols})
-        positions = gmocoin.position
+        isOrder = False
+    positions = gmocoin.position
+    if 'JPY' in positions:
         posJPY = positions['JPY']  # example for try
-    except Exception as e:
-        send_message_to_line(e)
-        send_message_to_line('Server Maintenance')
+    else:
         continue
 
-    for i in range(len(symbols)):
-        symbol = symbols[i]
-        try:
-            df = Tickers[i].history(period='3d', interval='15m')
-            df['+2σ'], df['MA1'], df['-2σ'] = BBANDS(df['Close'], timeperiod=stack1, matype=MA_Type.EMA)
-            df['MA2'] = MA(df['Close'], timeperiod=stack2)
-        except Exception as e:
-            send_message_to_line(e)
+    for i, symbol in enumerate(symbols):
+        df = Tickers[i].history(period='{period}d', interval='{interval}m')
+        if len(df['Close']) == 0:
+            del df
             continue
 
+        df['up'], df['MA1'], df['low'] = BBANDS(df['Close'],
+                                                timeperiod=stack1,
+                                                nbdevup=3,
+                                                nbdevup=3,
+                                                matype=MA_Type.EMA)
+        df['MA2'] = MA(df['Close'], timeperiod=stack2)
+        df['MA3'] = MA(df['Close'], timeperiod=stack3)
+
         price = df['Close'].iloc[-1]
+        orderPrice = calcPrice(symbol, price)
+        paa = priceAtAsk[i]
         MA1 = df['MA1'].iloc[-1]
         MA2 = df['MA2'].iloc[-1]
+        MA3 = df['MA3'].iloc[-1]
         lstMA1 = df['MA1'].iloc[-2]
         lstMA2 = df['MA2'].iloc[-2]
+        lstMA3 = df['MA3'].iloc[-2]
 
         if positions[symbol] != '0':
-            if 1.02 * priceAtAsk < price or \
-                    df['+2σ'].iloc[-1] < price and priceAtAsk < price or \
-                    MA1 < MA2 and priceAtAsk < price:
+            if df['up'].iloc[-1] < price and paa < price or \
+                    MA1 < MA2 and lstMA2 < lstMA1 and paa < price or \
+                    MA1 < MA3 and paa < price or \
+                    price < 0.9 * paa:
                 params = {
                     'symbol': symbol,
                     'side': 'SELL',
                     'executionType': 'LIMIT',
-                    'price': price,
+                    'price': orderPrice,
                     'size': positions[symbol]
                 }
                 gmocoin.order(params)
-                mpf.plot(df, mav=(stack1, stack2), style='yahoo', savefig=f'{symbol}.png')
+                isOrder = True
+                mpf.plot(df, mav=(stack1, stack2, stack3), type='line', style='yahoo', savefig=f'{symbol}.png')
         else:
-            if MA2 < MA1 and lstMA1 < lstMA2 and (derivative() or derivative2()) or \
-                    price < df['-2σ'].iloc[-1]:
-                size = str(int(float(posJPY)/price/4))  # maybe causes error 4 -> len(symbols)
+            if MA3 < MA1 and lstMA1 < lstMA3 or price < df['low'].iloc[-1]:
+                size = calcSize(symbol, float(posJPY)/price)
                 params = {
                     'symbol': symbol,
                     'side': 'BUY',
                     'executionType': 'LIMIT',
-                    'price': price,
+                    'price': orderPrice,
                     'size': size
                 }
                 gmocoin.order(params)
-                priceAtAsk = price
-                mpf.plot(df, mav=(stack1, stack2), style='yahoo', savefig=f'{symbol}.png')
+                priceAtAsk[i] = price
+                isOrder = True
+                mpf.plot(df, mav=(stack1, stack2, stack3), type='line', style='yahoo', savefig=f'{symbol}.png')
 
         del df
