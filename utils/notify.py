7cd0ba6b3c2f5779cdb858fad7577bb7ac16a2fb
YSK722 <56831867+YSK722@users.noreply.github.com>
2023-11-15 20:58:02 +0900

    Update notify.py

--- utils/notify.py
+++ utils/notify.py
@@ -1,4 +1,9 @@
 import requests
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+from socket import gaierror
+from urllib3.exceptions import NewConnectionError, MaxRetryError
 
 
 def pprint(message):
@@ -7,15 +12,27 @@ def pprint(message):
         for k, v in message.items():
             s += f'{k}:{v}\n'
         message = s
-    return '\n'+message
+    #return '\n'+message
+    return message
 
 
 def send_message_to_line(message):
+    line_bot_api = LineBotApi() # 自分のLINEのアクセストークン
+    try:
+        line_bot_api.push_message(,
+                                  TextSendMessage(text=pprint(message)))
+    except (LineBotApiError, requests.exceptions.ConnectionError, gaierror, NewConnectionError, MaxRetryError) as e:
+        send_message_to_line_old(message)
+
+def send_message_to_line_old(message):
     access_token = # 自分のLINEのアクセストークン
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
     data = {'message': pprint(message)}
-    requests.post(url='https://notify-api.line.me/api/notify',
-                  headers=headers,
-                  data=data)
+    try:
+        requests.post(url='https://notify-api.line.me/api/notify',
+                      headers=headers,
+                      data=data)
+    except (LineBotApiError, requests.exceptions.ConnectionError, gaierror, NewConnectionError, MaxRetryError) as e:
+        print(e)
